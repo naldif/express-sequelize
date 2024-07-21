0c74065fbbb615371c2abe2563472291eefa6374
Nito Aldif <nitoaldif@gmail.com>
2024-07-21 21:01:36 +0700

    add: read & detail category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,23 +1,20 @@
 const { Category } = require("../models")
 
-exports.getAllCategories = (req, res) => {
-    // console.log(req.requestTime)
-    res.status(200).json({
-        status: "success",
-        data: [{
-                "id": "01",
-                "name": "ipone"
-            },
-            {
-                "id": "02",
-                "name": "PC"
-            },
-            {
-                "id": "03",
-                "name": "Laptop"
-            },
-        ]
-    })
+exports.getAllCategories = async(req, res) => {
+   
+    try {
+        const categories = await Category.findAll();
+
+        return res.status(200).json({
+            status: "success",
+            data: categories
+        })
+    } catch (error) {
+        return res.status(500).json({
+            status: "fail",
+            error: "Server down"
+        })
+    }
 }
 
 exports.storeCategory = async(req, res) => {
@@ -31,7 +28,7 @@ exports.storeCategory = async(req, res) => {
         })
 
         res.status(200).json({
-            status: "Success",
+            status: "success",
             data: newCategory
         })
 
@@ -39,6 +36,28 @@ exports.storeCategory = async(req, res) => {
         return res.status(400).json({
             status: "fail",
             error: error.errors
+        })
+    }
+}
+
+exports.detailCategory = async(req, res) => {
+   
+    try {
+        const detailCategory = await Category.findByPk(req.params.id);
+        if(!detailCategory){
+            return res.status(404).json({
+                status: "fail",
+                message: "Data tidak di temukan"
+            })
+        }
+        res.status(200).json({
+            status: "success",
+            data: detailCategory
+        })
+    } catch (error) {
+        return res.status(500).json({
+            status: "fail",
+            error: 'server down'
         })
     }
 }
