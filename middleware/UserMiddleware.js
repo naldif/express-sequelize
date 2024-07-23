36d8e73ef5fe9c2280ea2a3710f186312c6d1634
Nito Aldif <nitoaldif@gmail.com>
2024-07-23 12:06:35 +0700

    add: protected route with jwt

---
+++ middleware/UserMiddleware.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken');
+const {
+    User
+} = require('../models');
+
+exports.authMiddleware = async (req, res, next) => {
+    // fungsi jika di header kita masukan token atau tidak
+
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(" ")[1]
+    }
+
+    if (!token) {
+        return next(res.status(401).json({
+            status: 401,
+            message: "anda belum login/register token tidak ditemukan"
+        }))
+    }
+
+    let decoded;
+    try {
+        decoded = await jwt.verify(token, process.env.JWT_SECRET)
+    } catch (err) {
+        return next(res.status(401).json({
+            error: err,
+            message: "Token yang di masukan tidak ditemukan/tidak ada"
+        }))
+    }
+
+    // ambil data user berdasarkan kondisi decoded
+    const currentUser = await User.findByPk(decoded.id)
+    console.log(currentUser)
+
+    req.user;
+
+    next()
+
+}
--- routes/categories.js
+++ routes/categories.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const { getAllCategories, storeCategory, detailCategory, updateCategory, deleteCategory } = require('../controllers/categoryController')
+const { authMiddleware } = require('../middleware/UserMiddleware')
 
 router.get('/', getAllCategories)
 
 router.post('/', storeCategory)
-router.get('/:id', detailCategory)
+router.get('/:id', authMiddleware, detailCategory)
 router.put('/:id', updateCategory)
 router.delete('/:id', deleteCategory)
 
