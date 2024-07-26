cd0be55afcf07f2f32fc5dcec63bb314228cc168
Nito Aldif <nitoaldif@gmail.com>
2024-07-26 10:03:20 +0700

    add: create data product

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,17 +10,30 @@ exports.addProduct = asyncHandle(async (req, res) => {
         price,
         categoryId,
         stock,
-        image
     } = req.body
+
+    const file = req.file;
+    // validasi jika file tidak di input
+    if(!file) {
+        res.status(400)
+        throw new Error("Tidak ada image yang di input")
+    }
+
+    const fileName = file.filename
+    const pathFile = `${req.protocol}://${req.get('host')}/public/uploads/${fileName}`
 
     const newProduct = await Product.create(
         {
             name,
-            description,
+            description, 
             price,
             categoryId,
             stock,
-            image,
+            image: pathFile,
         }
     )
+
+    return res.status(200).json({
+        data: newProduct
+    })
 })
